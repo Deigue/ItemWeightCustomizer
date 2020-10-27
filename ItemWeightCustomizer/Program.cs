b5cbcbb29fbbb4e1ff1d0195b0e887e244942a03
Deigue <deigue@hotmail.com>
2020-10-27 18:15:43 -0400

    Set up boiler-plate code for weight edits and config.

--- ItemWeightCustomizer/Program.cs
+++ ItemWeightCustomizer/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
+using System.IO;
 
 namespace ItemWeightCustomizer
 {
@@ -13,12 +14,47 @@ namespace ItemWeightCustomizer
         {
             return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
                 args: args,
-                patcher: RunPatch);
+                patcher: RunPatch,
+                new UserPreferences()
+                {
+                    ActionsForEmptyArgs = new RunDefaultPatcher
+                    {
+
+                        IdentifyingModKey = "ItemWeightCustomizer.esp",
+                        TargetRelease = GameRelease.SkyrimSE
+                    }
+                }
+            );
+        }
+
+        private static void SynthesisLog(string message, bool special = false)
+        {
+            if (special)
+            {
+                Console.WriteLine();
+                Console.Write(">>> ");
+            }
+            Console.WriteLine(message);
         }
 
         public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            //Your code here!
+            string configFilePath = Path.Combine(state.ExtraSettingsDataPath, "config.json");
+            if (!File.Exists(configFilePath))
+            {
+                var errorMessage = "Cannot find config.json for Custom Weights.";
+                var fileException = new FileNotFoundException(errorMessage, configFilePath);
+                SynthesisLog(errorMessage, true);
+                throw fileException;
+            }
+
+
+            Console.WriteLine("Running Item Weight Customizer ...");
+
+            foreach (var item in state.LoadOrder.PriorityOrder)
+            {
+                // iterate some items.
+            }
         }
     }
 }
