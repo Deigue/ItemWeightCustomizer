64e7c27c8fe673db05477df1a8f8634892dbdf28
Deigue <deigue@hotmail.com>
2020-12-08 22:20:28 -0500

    Refactored existing code and added existing options to README

--- ItemWeightCustomizer/Program.cs
+++ ItemWeightCustomizer/Program.cs
@@ -1,16 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
 using System.IO;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 
 namespace ItemWeightCustomizer
 {
-    public class Program
+    public static class Program
     {
         public static int Main(string[] args)
         {
@@ -40,10 +37,10 @@ namespace ItemWeightCustomizer
             if (special) Console.WriteLine();
         }
 
-        public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
+        private static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
         {
             string configFilePath = Path.Combine(state.ExtraSettingsDataPath, "config.json");
-            string errorMessage = "";
+            string errorMessage;
 
             if (!File.Exists(configFilePath))
             {
@@ -66,32 +63,30 @@ namespace ItemWeightCustomizer
                 throw new JsonSerializationException(errorMessage, jsonException);
             }
 
-            var bookWeight = config.getBookWeight();
-            var ingredientWeight = config.getIngredientWeight();
-            var scrollWeight = config.getScrollWeight();
-            var soulGemWeight = config.getSoulGemWeight();
+            var bookWeight = config.GetBookWeight();
+            var ingredientWeight = config.GetIngredientWeight();
+            var scrollWeight = config.GetScrollWeight();
+            var soulGemWeight = config.GetSoulGemWeight();
 
             // ***** PRINT CONFIG SETTINGS ***** //
-            SynthesisLog($"Item Weight Configuration:", true);
+            SynthesisLog("Item Weight Configuration:", true);
             if (bookWeight >= 0) SynthesisLog($"All books (BOOK) will have their weights set to {bookWeight}");
             if (ingredientWeight >= 0) SynthesisLog($"All ingredients (INGR) will have their weights set to {ingredientWeight}");
             if (scrollWeight >= 0) SynthesisLog($"All scrolls (SCRL) will have their weights set to {scrollWeight}");
             if (soulGemWeight >= 0) SynthesisLog($"All soul gems (SLGM) will have their weights set to {soulGemWeight}");
 
             // START WORK ...
-            Console.WriteLine("Running Item Weight Customizer ...");
+            SynthesisLog("Running Item Weight Customizer ...", true);
 
             // ***** BOOKS ***** //
             if (bookWeight >= 0)
             { 
                 foreach (IBookGetter book in state.LoadOrder.PriorityOrder.WinningOverrides<IBookGetter>())
                 {
-                    if (book.Weight != bookWeight)
-                    {
-                        var modifiedBook = book.DeepCopy();
-                        modifiedBook.Weight = bookWeight;
-                        state.PatchMod.Books.Add(modifiedBook);
-                    }
+                    if (Math.Abs(book.Weight - bookWeight) < float.Epsilon) continue;
+                    var modifiedBook = book.DeepCopy();
+                    modifiedBook.Weight = bookWeight;
+                    state.PatchMod.Books.Add(modifiedBook);
                 }
             }
 
@@ -100,12 +95,10 @@ namespace ItemWeightCustomizer
             {
                 foreach (IIngredientGetter ingredient in state.LoadOrder.PriorityOrder.WinningOverrides<IIngredientGetter>())
                 {
-                    if (ingredient.Weight != ingredientWeight)
-                    {
-                        var modifiedIngredient = ingredient.DeepCopy();
-                        modifiedIngredient.Weight = ingredientWeight;
-                        state.PatchMod.Ingredients.Add(modifiedIngredient);
-                    }
+                    if (Math.Abs(ingredient.Weight - ingredientWeight) < float.Epsilon) continue;
+                    var modifiedIngredient = ingredient.DeepCopy();
+                    modifiedIngredient.Weight = ingredientWeight;
+                    state.PatchMod.Ingredients.Add(modifiedIngredient);
                 }
             }
 
@@ -114,12 +107,10 @@ namespace ItemWeightCustomizer
             {
                 foreach (IScrollGetter scroll in state.LoadOrder.PriorityOrder.WinningOverrides<IScrollGetter>())
                 {
-                    if (scroll.Weight != scrollWeight)
-                    {
-                        var modifiedScroll = scroll.DeepCopy();
-                        modifiedScroll.Weight = scrollWeight;
-                        state.PatchMod.Scrolls.Add(modifiedScroll);
-                    }
+                    if (Math.Abs(scroll.Weight - scrollWeight) < float.Epsilon) continue;
+                    var modifiedScroll = scroll.DeepCopy();
+                    modifiedScroll.Weight = scrollWeight;
+                    state.PatchMod.Scrolls.Add(modifiedScroll);
                 }
             }
 
@@ -128,12 +119,10 @@ namespace ItemWeightCustomizer
             {
                 foreach (ISoulGemGetter soulGem in state.LoadOrder.PriorityOrder.WinningOverrides<ISoulGemGetter>())
                 {
-                    if (soulGem.Weight != soulGemWeight)
-                    {
-                        var modifiedSoulGem = soulGem.DeepCopy();
-                        modifiedSoulGem.Weight = soulGemWeight;
-                        state.PatchMod.SoulGems.Add(modifiedSoulGem);
-                    }
+                    if (Math.Abs(soulGem.Weight - soulGemWeight) < float.Epsilon) continue;
+                    var modifiedSoulGem = soulGem.DeepCopy();
+                    modifiedSoulGem.Weight = soulGemWeight;
+                    state.PatchMod.SoulGems.Add(modifiedSoulGem);
                 }
             }
             
