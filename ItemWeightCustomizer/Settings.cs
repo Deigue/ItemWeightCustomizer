361401117822044a1107c86efbed51c911bb4b22
profaneblood <carlgon@tutanota.com>
2021-04-02 20:44:11 -0400

    Weights set by multiplication, settings tab enabled, more item categories added.

---
+++ ItemWeightCustomizer/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace ItemWeightCustomizer
+{
+    public partial class Settings
+    {
+        [JsonProperty("weightSettings")]
+        public WeightSettings WeightSettings { get; set; } = new WeightSettings();
+
+        [JsonProperty("categories")]
+        public HashSet<WeightCategory> Categories { get; set; } = new HashSet<WeightCategory>();
+    }
+}
